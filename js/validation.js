2c79b8b621155b24d809a52b6d354a4d511070ef
TousTarm <tomas.rindt@gmail.com>
2023-06-05 00:17:49 +0200

    commit 7

--- js/validation.js
+++ js/validation.js
@@ -1,11 +1,10 @@
 const validation = new JustValidate("#signupform");
 
 validation
-    .addField("#nickname",[{rule:"required"}])/*
+    .addField("#nickname",[{rule:"required"}])
     .addField("#email",[{rule:"required"},{rule:"email"}])
     .addField("#password",[{rule:"required"},{rule:"password"}])
     .addField("#password2",[{validator:(value, fields) => {return value === fields["#password"].elem.value;}, errorMessage: "Passwords do not match"}])
-    */
     .onSuccess((event) => {
         document.getElementById("signupform").submit();
     });
